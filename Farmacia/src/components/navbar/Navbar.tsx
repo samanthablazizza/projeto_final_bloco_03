6b10544016cdb1e65ce6f90c3f52d63e61c2548b
samanthablazizza <samantha.blazizza@gmail.com>
2023-11-09 10:30:20 -0300

    Implementação dos componentes Home, Navbar e Footer

---
+++ Farmacia/src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmácia Amiga
+
+                    <div className='flex gap-4'>
+                        Home
+                        Categorias
+                        Produtos
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ Farmacia/src/pages/home/Home.tsx
@@ -0,0 +1,36 @@
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+                        <p className='text-xl'>
+                            Farmácia Amiga: Aqui há os melhores preços!
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                            >
+                                Buscar
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://seeklogo.com/images/F/farmacia-popular-logo-EFFEFB5330-seeklogo.com.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
