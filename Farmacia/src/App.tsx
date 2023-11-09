f28b4da99c2ecdfdf581f112dc459d0e15954186
samanthablazizza <samantha.blazizza@gmail.com>
2023-11-09 14:18:20 -0300

    Implementação das rotas

--- Farmacia/src/App.tsx
+++ Farmacia/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <div className='min-h-[80vh]'>
         <Routes>
+        <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/categorias" element={<ListaCategorias />} />
           <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
--- Farmacia/src/components/navbar/Navbar.tsx
+++ Farmacia/src/components/navbar/Navbar.tsx
@@ -5,12 +5,12 @@ function Navbar() {
         <>
             <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
-            
+
                 <div className="container flex justify-between text-lg">
                     Farmácia Amiga
 
                     <div className='flex gap-4'>
-                        Home
+                        <Link to='/home' className='hover:underline'>Home</Link>
                         <Link to='/categorias' className='hover:underline'>Categorias</Link>
                         <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar categoria</Link>
                         Produtos
